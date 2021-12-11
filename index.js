b5afbcb39a6f6adc2f020b0f2235d0d8085bc135
waki285 <suzuneu@suzuneu.com>
2021-12-11 04:29:03 +0000

    ✨ feat: add command

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, Permissions, MessageButton, MessageEmbed, MessageActionRow } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES], partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"], restTimeOffset: 50 });
 const { default:axios } = require("axios");
+const { randomBytes } = require("crypto");
 
 client.on("ready", () => console.log(`Logged in as ${client.user.tag}.`));
+
+client.on("messageCreate", (message) => {
+  if (message.content === "!scratchauth" && message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+    const embed = new MessageEmbed()
+      .setTitle("Scratch認証")
+      .setDescription("下のボタンを押して、ScratchのアカウントとDiscordアカウントの紐付けを開始してください。")
+      .setColor("GREEN");
+    const button = new MessageButton()
+      .setCustomId("verify")
+      .setStyle("SUCCESS")
+      .setLabel("認証");
+    message.channel.send({ embeds: [embed], components: [new MessageActionRow().addComponents(button)] });
+  }
+})
